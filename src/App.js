631ff3ef602d2959930dec79ffdb599e197a5eff
Yash Raj <86347034+yxshrraj@users.noreply.github.com>
2023-07-21 19:28:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,61 +1,64 @@
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-import{ useEffect, useState} from"react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-const apiKey ="234f2a9ac55d9467ca6e563c170e62ff"
-const [inputCity,setInputCity] =useState("")
-const [data, setData]= useState({}) 
- const getWeatherDetails = (cityName) =>{
-  if(!cityName) return
-  const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName +" &appid=" + apiKey
-  axios.get(apiURL).then((res)=>{
-    console.log("response",res.data)
-    setData(res.data)
-  }).catch((err)=>{
-    console.log("err",err)
-  })
- }
+  const apiKey = "234f2a9ac55d9467ca6e563c170e62ff";
+  const [inputCity, setInputCity] = useState("");
+  const [data, setData] = useState({});
 
- const handleChangeInput= (e) =>
- {
-      console.log("value",e.target.value)
-      setInputCity(e.target.value)
- }
- const handleSearch = () =>{ 
-  getWeatherDetails(inputCity)
+  const getWeatherDetails = (cityName) => {
+    if (!cityName) return;
+    const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+    axios.get(apiURL).then((res) => {
+      console.log("response", res.data);
+      setData(res.data);
+    }).catch((err) => {
+      console.log("err", err);
+    });
+  };
 
- }
+  const handleChangeInput = (e) => {
+    console.log("value", e.target.value);
+    setInputCity(e.target.value);
+  };
 
+  const handleSearch = () => {
+    getWeatherDetails(inputCity);
+  };
 
   return (
-   <div className="col-md-12">
-    <div className="weatherbg">
-      <h1 className="heading">Weather App</h1>
+    <div className="col-md-12" style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+      <div className="weatherbg">
+        <h1 className="heading">Weather App</h1>
 
-       <div className="d-grid gap-3 col-4 mt-4">
-        <input type="text" className="form-control" 
-        value ={inputCity} onChange={handleChangeInput} />
-         <button className="btn  btn-primary" type="button" 
-         onClick={handleSearch}>Search</button>
-       </div>
-    </div>
-    {Object.keys(data).length>0 &&
-    <div className="col-md-12 text-center mt-5">
-      <div className="shadow rounded weatherResultBox">
-        <img className="weathericon" src="https://w7.pngwing.com/pngs/1005/263/png-transparent-weather-forecasting-logo-weather-blue-game-cloud-thumbnail.png" />
-
-        <h5 className="weathercity">
-          {data?.name}
-        </h5>
-
-        <h6 className="weathertemp">{((data?.main?.temp)-273.15).toFixed(2)}°C</h6>
+        <div className="d-grid gap-3 col-4 mt-4" >
+          <input type="text" className="form-control"
+            value={inputCity} onChange={handleChangeInput} />
+          <button className="btn btn-primary" type="button"
+            onClick={handleSearch}>Search</button>
+        </div>
       </div>
+      {Object.keys(data).length > 0 &&
+        <div className="col-md-12 text-center mt-5" >
+          <div className="shadow rounded weatherResultBox" >
+            <img className="weathericon" src="https://w7.pngwing.com/pngs/1005/263/png-transparent-weather-forecasting-logo-weather-blue-game-cloud-thumbnail.png" />
 
+            <h5 className="weathercity">
+              {data?.name}
+            </h5>
+
+            <h6 className="weathertemp">{((data?.main?.temp) - 273.15).toFixed(2)}°C</h6>
+          </div>
+          
+        </div>
+      }
+      {/* Footer */}
+      <footer className="text-center mt-auto" style={{ backgroundColor: 'lightgrey', padding: '10px' }}>
+        Made with ❤️ by Yash Raj
+      </footer>
     </div>
-}
-   </div> 
   );
 }
 
